c33e8585247c6051fbecd5a3521bcb202059cf5f
Rob Dawson <rob@codebox.org.uk>
2021-10-02 20:12:31 +0100

    sidewinder algorithm, dijkstra for distance

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ function buildGrid(width, height) {
     const cells = [...Array(height)].map((row, y) => [...Array(width)].map((_, x) => {
         return {
             neighbours: {},
+            metadata: {},
             northEdge: y === 0,
             southEdge: y === height - 1,
             eastEdge: x === width - 1,
@@ -18,12 +19,31 @@ function buildGrid(width, height) {
             }
         },
         forEachCell(fn) {
-            for (let x = 0; x < width; x++) {
-                for (let y = 0; y < height; y++) {
+            for (let y = 0; y < height; y++) {
+                for (let x = 0; x < width; x++) {
                     fn(this.getCell(x, y), x, y);
                 }
             }
-        },cells,
+        },
+        findDistancesFrom(x, y) {
+            const startCell = this.getCell(x, y);
+            startCell.metadata.distance = 0;
+            const frontier = [startCell];
+            let maxDistance = 0;
+            while(frontier.length) {
+                const next = frontier.shift(),
+                    frontierDistance = next.metadata.distance;
+                const linkedUndistancedNeighbours = Object.values(next.neighbours).filter(neighbour => neighbour.link).filter(neighbour => neighbour.cell.metadata.distance === undefined).map(neighbour => neighbour.cell);
+                linkedUndistancedNeighbours.forEach(neighbour => {
+                    neighbour.metadata.distance = frontierDistance + 1;
+                });
+                frontier.push(...linkedUndistancedNeighbours);
+                maxDistance = Math.max(frontierDistance+1, maxDistance);
+            }
+            this.metadata.maxDistance = maxDistance;
+        },
+        cells, //TODO remove
+        metadata: {},
         width,height
     };
 
@@ -60,12 +80,44 @@ function generateMazeBinaryTree(grid) {
             }
         }
     }
-    console.log(grid.cells)
     return grid;
 }
+
+function generateMazeSidewinder(grid) {
+    "use strict";
+    for (let y = 0; y < grid.height; y++) {
+        let runLength = 0;
+        for (let x = 0; x < grid.width; x++) {
+            runLength++;
+            const cell = grid.getCell(x, y),
+                rnd = Math.random(),
+                removeEastWall = (rnd < 0.5 || cell.southEdge) && !cell.eastEdge;
+
+            if (removeEastWall) {
+                const neighbour = cell.neighbours.east.cell;
+                cell.neighbours.east.link = true;
+                console.assert(neighbour.neighbours.west.cell === cell && neighbour.neighbours.west.link === false);
+                neighbour.neighbours.west.link = true;
+
+            } else if (!cell.southEdge) {
+                console.log('end of run ', runLength, x, y)
+                const randomCellFromRun = grid.getCell(x - Math.floor(Math.random() * runLength), y);
+                const neighbour = randomCellFromRun.neighbours.south.cell;
+                randomCellFromRun.neighbours.south.link = true;
+                console.assert(neighbour.neighbours.north.cell === randomCellFromRun && neighbour.neighbours.north.link === false);
+                neighbour.neighbours.north.link = true;
+
+                runLength = 0;
+            }
+        }
+    }
+    return grid;
+}
+
 function generateMaze(grid) {
     "use strict";
-    return generateMazeBinaryTree(grid);
+    // return generateMazeBinaryTree(grid);
+    return generateMazeSidewinder(grid);
 }
 
 function render(maze) {
@@ -79,8 +131,14 @@ function render(maze) {
         ctx.moveTo(x0 * MAGNIFICATION, y0 * MAGNIFICATION);
         ctx.lineTo(x1 * MAGNIFICATION, y1 * MAGNIFICATION);
     }
-
+    function drawRectangle(x, y, distance) {
+        ctx.moveTo(x * MAGNIFICATION, y * MAGNIFICATION);
+        //ctx.fillStyle = `rgba(0, 0, 255, ${distance})`;
+        ctx.fillStyle = `hsl(${Math.floor(100 * distance)}, 100%, 50%)`;
+        ctx.fillRect(x * MAGNIFICATION, y * MAGNIFICATION, MAGNIFICATION, MAGNIFICATION);
+    }
     function renderCell(x, y, cell) {
+        drawRectangle(x,y,cell.metadata.distance/maze.metadata.maxDistance);
         if (!cell.neighbours.north.link) {
             drawWall(x, y, x+1, y);
         }
@@ -112,5 +170,7 @@ window.onload = () => {
     const grid = buildGrid(30,30),
         maze = generateMaze(grid);
 
+    maze.findDistancesFrom(0,0);
     render(maze);
+    console.log(maze.cells)
 };
